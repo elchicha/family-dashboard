7b469005b792cdefe416e67e1aeaffbb2dd51717
elchicha <github.com.bulginess438@passmail.com>
2026-01-13 06:30:40 -0800

    feat: implement add_widget method for Dashboard

--- src/dashboard.py
+++ src/dashboard.py
@@ -5,3 +5,7 @@ class Dashboard:
 
     def __init__(self, display: DisplayInterface):
         self.display = display
+        self.widgets = []
+
+    def add_widget(self, widget, x_pos: int, y_pos: int):
+        self.widgets.append((widget, x_pos, y_pos))
