df99c7e9592ad44665920fba5c1e70228dc11983
elchicha <github.com.bulginess438@passmail.com>
2026-01-18 12:26:20 -0800

    feat: implement basic add_widget method

--- src/layout/three_column_layout.py
+++ src/layout/three_column_layout.py
@@ -4,3 +4,7 @@ from src.layout.layout_interface import LayoutInterface
 class ThreeColumnLayout(LayoutInterface):
     def __init__(self):
         self.columns = [[], [], []]
+        self.column_width = width // 3
+
+    def add_widget(self, widget, column):
+        self.columns[column].append(widget)
