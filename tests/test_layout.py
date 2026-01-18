fc759f70c7584c427474c58d0c6a6bc2a284ed4f
elchicha <github.com.bulginess438@passmail.com>
2026-01-18 12:25:55 -0800

    test: add test for column width and add widget to column

--- tests/test_layout.py
+++ tests/test_layout.py
@@ -16,3 +16,19 @@ class TestThreeColumnLayout:
     def test_layout_has_three_columns(self, layout):
         """Layout should divide the screen into 3 columns"""
         assert len(layout.columns) == 3
+
+    def test_layout_calculates_column_widths(self):
+        """Each column should have equal width based on screen size"""
+        layout = ThreeColumnLayout(width=1872, height=1404)
+
+        assert layout.column_width == 624
+
+    def test_add_widget_to_column(self, layout):
+        """Should be able to add widgets to specific columns"""
+        from src.widgets.clock_widget import ClockWidget
+
+        widget = ClockWidget()
+        layout.add_widget(widget, column=0)
+
+        assert len(layout.columns[0]) == 1
+        assert layout.columns[0][0] == widget
