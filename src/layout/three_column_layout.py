c1818606c17df810d1c01bb80e668dc8549aa0b3
elchicha <github.com.bulginess438@passmail.com>
2026-01-18 12:27:19 -0800

    refactor: change class signature to include width , height params

--- src/layout/three_column_layout.py
+++ src/layout/three_column_layout.py
@@ -2,7 +2,9 @@ from src.layout.layout_interface import LayoutInterface
 
 
 class ThreeColumnLayout(LayoutInterface):
-    def __init__(self):
+    def __init__(self, width=1872, height=1404):
+        self.width = width
+        self.height = height
         self.columns = [[], [], []]
         self.column_width = width // 3
 
